1acfc7b0abbc82205cf3d92627d64deb52a5342b
Jens Zonneveld <zuperjens@gmail.com>
2015-10-29 17:26:52 +0100

    added comment to the lines

--- rt-scripts.sh
+++ rt-scripts.sh
@@ -3,9 +3,8 @@ echo Starting the rt test script
 
 DATELINE=$(date +%Y-%m-%d_%H:%M)
 
+#check for cyclictest
 command -v cyclictest >/dev/null 2>&1 || { echo >&2 "I require cyclictest but it's not installed. Please install."; exit 1; }
 
+#start with unloaded test on 500 ns
 cyclictest -t1 -p 80 -i 500 -n -l 1000000 -h 100 -q >> "unloaded_$DATELINE.log"
-
-
-
